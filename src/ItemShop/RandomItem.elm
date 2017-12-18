cf39b0441eea45af0d5400a4ab4c86f200ac75a4
David Gabor Bodor <david.gabor.bodor@gmail.com>
2017-12-18 12:56:26 +0100

    generate random modifier for item

--- src/ItemShop/RandomItem.elm
+++ src/ItemShop/RandomItem.elm
@@ -1,18 +1,61 @@
-module ItemShop.RandomItem exposing (randomItem)
+module ItemShop.RandomItem exposing (randomItem, randomModifiers)
 
 import Array exposing (Array, fromList)
-import Maybe exposing (withDefault)
-import Random exposing (Generator)
+import Html exposing (a)
 import ItemShop.Item exposing (Item, nothing)
 import ItemShop.Items exposing (baseItems)
+import ItemShop.Modifier exposing (Modifier, regular)
+import ItemShop.Modifiers exposing (allModifiers)
+import Maybe exposing (withDefault)
+import Random exposing (Generator)
 
 
 randomChoice : a -> Array a -> Generator a
 randomChoice defaultItem items =
     Random.map (\i -> Array.get i items |> withDefault defaultItem)
-        (Random.int 0 (Array.length items))
+        (Random.int 0 ((Array.length items) - 1))
 
 
 randomItem : Generator Item
 randomItem =
+    Random.map2 (\item mods -> { item | modifiers = mods })
+        randomBaseItem
+        randomModifiers
+
+
+randomBaseItem : Generator Item
+randomBaseItem =
     randomChoice nothing (fromList baseItems)
+
+
+modifierOrEmpty : Float -> Modifier -> Generator (List Modifier)
+modifierOrEmpty probability modifier =
+    Random.float 0.0 1.0
+        |> Random.map
+            (\f ->
+                if (f < probability) then
+                    [ modifier ]
+                else
+                    []
+            )
+
+
+randomConst : a -> Generator a
+randomConst a =
+    Random.map (\b -> a) Random.bool
+
+
+randomModifiers : Generator (List Modifier)
+randomModifiers =
+    let
+        probability =
+            1.0 / (toFloat <| List.length <| allModifiers)
+
+        generators : List (Generator (List Modifier))
+        generators =
+            List.map (modifierOrEmpty probability) allModifiers
+
+        emptyList =
+            Random.list 0 (randomConst regular)
+    in
+        List.foldr (Random.map2 List.append) emptyList generators
