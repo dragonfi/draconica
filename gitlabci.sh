42c50d7f1b57667de9c53aeff50083606fa41157
David Gabor Bodor <david.gabor.bodor@gmail.com>
2017-12-19 12:28:37 +0100

    build elm with gitlab ci

---
+++ gitlabci.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+set -x
+
+gitlab-ci-multi-runner exec docker pages
--- make.sh
+++ make.sh
@@ -5,7 +5,7 @@ set -x
 rm -r target
 mkdir -p target
 
-elm make --output target/draconica.html src/Draconica.elm
-elm make --output target/itemshop.html src/ItemShop.elm
+elm make --yes --output target/draconica.html src/Draconica.elm
+elm make --yes --output target/itemshop.html src/ItemShop.elm
 
 cp static/* target/
