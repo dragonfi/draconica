39a5785176d9aa8da3f3da916492dcaea69488b7
David Gabor Bodor <david.gabor.bodor@gmail.com>
2017-12-19 13:38:14 +0100

    add push-pages.sh

---
+++ push-pages.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+set -x
+
+MSG="$(git log -1 --pretty=%B)"
+
+cd target/
+git status
+
+read -p "Continue (y/n)?" CONT
+if [ "$CONT" = "y" ]; then
+  echo "Pushing...";
+else
+  echo "Exiting...";
+  exit 1
+fi
+
+git add -A
+git commit -am "$MSG"
+git push
